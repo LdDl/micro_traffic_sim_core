a9b443c07f337a3ddbbefc880f9f7f713b5c52da
LdDl <sexykdi@gmail.com>
2024-12-24 16:17:08 +0300

    add zone_type

---
+++ src/grid/zones.rs
@@ -0,0 +1,31 @@
+use std::fmt;
+
+/// ZoneType gives meaning to the cell in terms of its application.
+#[derive(Debug, Clone, Copy)]
+pub enum ZoneType {
+    Undefined = 0,
+    Birth,
+    Death,
+    Coordination,
+    Common,
+    Isolated,
+    LaneForBus,
+    Transit,
+    Crosswalk,
+}
+
+impl fmt::Display for ZoneType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ZoneType::Undefined => write!(f, "undefined"),
+            ZoneType::Birth => write!(f, "birth"),
+            ZoneType::Death => write!(f, "death"),
+            ZoneType::Coordination => write!(f, "coordination"),
+            ZoneType::Common => write!(f, "common"),
+            ZoneType::Isolated => write!(f, "isolated"),
+            ZoneType::LaneForBus => write!(f, "bus_lane"),
+            ZoneType::Transit => write!(f, "transit"),
+            ZoneType::Crosswalk => write!(f, "crosswalk"),
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,3 @@
 //! Export library
 pub mod geom;
-
+pub mod grid;
