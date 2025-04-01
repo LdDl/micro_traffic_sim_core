4f61a4040da208b96277eeb00bdc5d219cac3018
Dimitrii Lopanov <sexykdi@gmail.com>
2025-04-01 14:35:01 +0300

    describe basic rules and resolve it + test

--- src/conflicts/conflict_rule.rs
+++ src/conflicts/conflict_rule.rs
@@ -5,7 +5,6 @@ use crate::grid::lane_change_type::LaneChangeType;
 use crate::intentions::{CellIntention, IntentionType};
 use rand::Rng;
 
-// At the top of your file
 #[cfg(not(test))]
 use rand::thread_rng;
 
@@ -17,6 +16,95 @@ pub fn thread_rng() -> impl Rng {
 }
 
 const EPS_COOP_LEVEL: f64 = 0.0001;
+
+// Define the structure with function pointers
+struct ConflictRule {
+    condition: fn(&Vehicle, &Vehicle) -> bool,
+    resolver: for<'a> fn(
+        &'a CellIntention<'a>,
+        &'a CellIntention<'a>,
+    ) -> (&'a CellIntention<'a>, ConflictType),
+}
+
+// Create a static array of rules
+static CONFLICT_RULES: &[ConflictRule] = &[
+    ConflictRule {
+        // Both vehicles are changing lane on a single road
+        // Two source lanes on one road is going to merge into single lane on another road
+        condition: |v1, v2| changing_lane(v1) && changing_lane(v2),
+        resolver: |cin1, cin2| resolve_merge_lane_change(cin1, cin2),
+    },
+    ConflictRule {
+        // Both vehicles are moving forward on different lanes of different roads
+        // Differet roads are going to merge into single road
+        condition: |v1, v2| {
+            v1.intention.intention_maneuver == LaneChangeType::NoChange
+                && v2.intention.intention_maneuver == LaneChangeType::NoChange
+        },
+        resolver: |cin1, cin2| resolve_merge_forward(cin1, cin2),
+    },
+    ConflictRule {
+        // First vehicle is moving forward, second is changing lane
+        condition: |v1, v2| {
+            v1.intention.intention_maneuver == LaneChangeType::NoChange && changing_lane(v2)
+        },
+        resolver: |cin1, cin2| {
+            // First vehicle is not doing maneuver, when the second one is doing lane change.
+            // Therefore the second vehicle should give way to the first one
+            (cin1, ConflictType::MergeForward)
+        },
+    },
+    ConflictRule {
+        // First vehicle is changing lane, second is moving forward
+        condition: |v1, v2| {
+            changing_lane(v1) && v2.intention.intention_maneuver == LaneChangeType::NoChange
+        },
+        resolver: |cin1, cin2| {
+            // Second vehicle is not doing maneuver, when the first one is doing lane change.
+            // Therefore the first vehicle should give way to the second one
+            (cin2, ConflictType::MergeForward)
+        },
+    },
+    ConflictRule {
+        // First vehicle is changing lane, second is blocking its lane
+        condition: |v1, v2| {
+            changing_lane(v1) && v2.intention.intention_maneuver == LaneChangeType::Block
+        },
+        resolver: |cin1, cin2| {
+            // Second vehicle is not moving therefore it holds the position
+            (cin2, ConflictType::BlockLaneChange)
+        },
+    },
+    ConflictRule {
+        // First vehicle is blocking its lane, second is changing lane
+        condition: |v1, v2| {
+            v1.intention.intention_maneuver == LaneChangeType::Block && changing_lane(v2)
+        },
+        resolver: |cin1, cin2| {
+            // First vehicle is not moving therefore it holds the position
+            (cin1, ConflictType::BlockLaneChange)
+        },
+    },
+];
+
+// Function to resolve conflicts
+pub fn resolve_simple_rules<'a>(
+    intention_one: &'a CellIntention<'a>,
+    intention_two: &'a CellIntention<'a>,
+) -> (&'a CellIntention<'a>, ConflictType) {
+    let v1 = intention_one
+        .get_vehicle()
+        .expect("Vehicle missing in intention_one");
+    let v2 = intention_two
+        .get_vehicle()
+        .expect("Vehicle missing in intention_two");
+    for rule in CONFLICT_RULES {
+        if (rule.condition)(v1, v2) {
+            return (rule.resolver)(intention_one, intention_two);
+        }
+    }
+    panic!("Unexpected conflict type")
+}
 
 /// Checks if the vehicle is changing lane
 pub fn changing_lane(agent: &Vehicle) -> bool {
@@ -345,6 +433,202 @@ mod tests {
         let intention_six = CellIntention::new(Some(&vehicle_six), IntentionType::Target);
         let correct_winner = (intention_five.clone(), ConflictType::MergeForward);
         let actual_winner = resolve_merge_forward(&intention_five, &intention_six);
+        assert_eq!(
+            correct_winner.0.get_vehicle().unwrap().id,
+            actual_winner.0.get_vehicle().unwrap().id,
+            "Vehicle ID for the winner is not correct"
+        );
+        assert_eq!(
+            correct_winner.1, actual_winner.1,
+            "Conflict type is not correct"
+        );
+    }
+    #[test]
+    fn test_resolve_simple_rules() {
+        // Case 1: both vehicles are changing lane on a single road
+        // Two source lanes on one road is going to merge into single lane on another road
+        // Winner: vehicle who is doing LEFT maneuver
+        let mut vehicle_one = Vehicle::new(1)
+            .with_behaviour(BehaviourType::Undefined)
+            .with_speed(3)
+            .build();
+        vehicle_one.set_intention(VehicleIntention {
+            intention_maneuver: LaneChangeType::ChangeRight,
+            ..Default::default()
+        });
+        let mut vehicle_two = Vehicle::new(2)
+            .with_behaviour(BehaviourType::Undefined)
+            .with_speed(3)
+            .build();
+        vehicle_two.set_intention(VehicleIntention {
+            intention_maneuver: LaneChangeType::ChangeLeft,
+            ..Default::default()
+        });
+        let intention_one = CellIntention::new(Some(&vehicle_one), IntentionType::Target);
+        let intention_two = CellIntention::new(Some(&vehicle_two), IntentionType::Target);
+        let correct_winner = (intention_two.clone(), ConflictType::MergeLaneChange);
+        let actual_winner = resolve_simple_rules(&intention_one, &intention_two);
+        assert_eq!(
+            correct_winner.0.get_vehicle().unwrap().id,
+            actual_winner.0.get_vehicle().unwrap().id,
+            "Vehicle ID for the winner is not correct"
+        );
+        assert_eq!(
+            correct_winner.1, actual_winner.1,
+            "Conflict type is not correct"
+        );
+
+        // Case 2: both vehicles are moving forward on different lanes of different roads
+        // Differet roads are going to merge into single road
+        // Winner: vehicle who is moving faster
+        let mut vehicle_three = Vehicle::new(3)
+            .with_behaviour(BehaviourType::Undefined)
+            .with_speed(5)
+            .build();
+        vehicle_three.set_intention(VehicleIntention {
+            intention_maneuver: LaneChangeType::NoChange,
+            ..Default::default()
+        });
+        let mut vehicle_four = Vehicle::new(4)
+            .with_behaviour(BehaviourType::Undefined)
+            .with_speed(3)
+            .build();
+        vehicle_four.set_intention(VehicleIntention {
+            intention_maneuver: LaneChangeType::NoChange,
+            ..Default::default()
+        });
+        let intention_three = CellIntention::new(Some(&vehicle_three), IntentionType::Target);
+        let intention_four = CellIntention::new(Some(&vehicle_four), IntentionType::Target);
+        let correct_winner = (intention_three.clone(), ConflictType::MergeForward);
+        let actual_winner = resolve_simple_rules(&intention_three, &intention_four);
+        assert_eq!(
+            correct_winner.0.get_vehicle().unwrap().id,
+            actual_winner.0.get_vehicle().unwrap().id,
+            "Vehicle ID for the winner is not correct"
+        );
+        assert_eq!(
+            correct_winner.1, actual_winner.1,
+            "Conflict type is not correct"
+        );
+
+        // Case 3: First vehicle is moving forward, second is changing lane
+        // Winner: first vehicle (who is moving forward)
+        let mut vehicle_five = Vehicle::new(5)
+            .with_behaviour(BehaviourType::Undefined)
+            .with_speed(3)
+            .build();
+        vehicle_five.set_intention(VehicleIntention {
+            intention_maneuver: LaneChangeType::NoChange,
+            ..Default::default()
+        });
+        let mut vehicle_six = Vehicle::new(6)
+            .with_behaviour(BehaviourType::Undefined)
+            .with_speed(3)
+            .build();
+        vehicle_six.set_intention(VehicleIntention {
+            intention_maneuver: LaneChangeType::ChangeRight,
+            ..Default::default()
+        });
+        let intention_five = CellIntention::new(Some(&vehicle_five), IntentionType::Target);
+        let intention_six = CellIntention::new(Some(&vehicle_six), IntentionType::Target);
+        let correct_winner = (intention_five.clone(), ConflictType::MergeForward);  
+        let actual_winner = resolve_simple_rules(&intention_five, &intention_six);
+        assert_eq!(
+            correct_winner.0.get_vehicle().unwrap().id,
+            actual_winner.0.get_vehicle().unwrap().id,
+            "Vehicle ID for the winner is not correct"
+        );
+        assert_eq!(
+            correct_winner.1, actual_winner.1,
+            "Conflict type is not correct"
+        );
+
+        // Case 4: First vehicle is changing lane, second is moving forward
+        // Winner: second vehicle (who is moving forward)
+        let mut vehicle_seven = Vehicle::new(7)
+            .with_behaviour(BehaviourType::Undefined)
+            .with_speed(3)
+            .build();
+        vehicle_seven.set_intention(VehicleIntention {
+            intention_maneuver: LaneChangeType::ChangeRight,
+            ..Default::default()
+        });
+        let mut vehicle_eight = Vehicle::new(8)
+            .with_behaviour(BehaviourType::Undefined)
+            .with_speed(3)
+            .build();
+        vehicle_eight.set_intention(VehicleIntention {
+            intention_maneuver: LaneChangeType::NoChange,
+            ..Default::default()
+        });
+        let intention_seven = CellIntention::new(Some(&vehicle_seven), IntentionType::Target);
+        let intention_eight = CellIntention::new(Some(&vehicle_eight), IntentionType::Target);
+        let correct_winner = (intention_eight.clone(), ConflictType::MergeForward);
+        let actual_winner = resolve_simple_rules(&intention_seven, &intention_eight);
+        assert_eq!(
+            correct_winner.0.get_vehicle().unwrap().id,
+            actual_winner.0.get_vehicle().unwrap().id,
+            "Vehicle ID for the winner is not correct"
+        );
+        assert_eq!(
+            correct_winner.1, actual_winner.1,
+            "Conflict type is not correct"
+        );
+
+        // Case 5: First vehicle is changing lane, second is blocking its lane
+        // Winner: second vehicle (who is blocking the lane)
+        let mut vehicle_nine = Vehicle::new(9)
+            .with_behaviour(BehaviourType::Undefined)
+            .with_speed(3)
+            .build();
+        vehicle_nine.set_intention(VehicleIntention {
+            intention_maneuver: LaneChangeType::ChangeLeft,
+            ..Default::default()
+        });
+        let mut vehicle_ten = Vehicle::new(10)
+            .with_behaviour(BehaviourType::Undefined)
+            .with_speed(3)
+            .build();
+        vehicle_ten.set_intention(VehicleIntention {
+            intention_maneuver: LaneChangeType::Block,
+            ..Default::default()
+        });
+        let intention_nine = CellIntention::new(Some(&vehicle_nine), IntentionType::Target);
+        let intention_ten = CellIntention::new(Some(&vehicle_ten), IntentionType::Target);
+        let correct_winner = (intention_ten.clone(), ConflictType::BlockLaneChange);
+        let actual_winner = resolve_simple_rules(&intention_nine, &intention_ten);
+        assert_eq!(
+            correct_winner.0.get_vehicle().unwrap().id,
+            actual_winner.0.get_vehicle().unwrap().id,
+            "Vehicle ID for the winner is not correct"
+        );
+        assert_eq!(
+            correct_winner.1, actual_winner.1,
+            "Conflict type is not correct"
+        );
+
+        // Case 6: First vehicle is blocking its lane, second is changing lane
+        // Winner: first vehicle (who is blocking the lane)
+        let mut vehicle_eleven = Vehicle::new(11)
+            .with_behaviour(BehaviourType::Undefined)
+            .with_speed(3)
+            .build();
+        vehicle_eleven.set_intention(VehicleIntention {
+            intention_maneuver: LaneChangeType::Block,
+            ..Default::default()
+        });
+        let mut vehicle_twelve = Vehicle::new(12)
+            .with_behaviour(BehaviourType::Undefined)
+            .with_speed(3)
+            .build();
+        vehicle_twelve.set_intention(VehicleIntention {
+            intention_maneuver: LaneChangeType::ChangeRight,
+            ..Default::default()
+        });
+        let intention_eleven = CellIntention::new(Some(&vehicle_eleven), IntentionType::Target);
+        let intention_twelve = CellIntention::new(Some(&vehicle_twelve), IntentionType::Target);
+        let correct_winner = (intention_eleven.clone(), ConflictType::BlockLaneChange);
+        let actual_winner = resolve_simple_rules(&intention_eleven, &intention_twelve);
         assert_eq!(
             correct_winner.0.get_vehicle().unwrap().id,
             actual_winner.0.get_vehicle().unwrap().id,
