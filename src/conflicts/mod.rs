dc88d99a309b3a630da0bc78f9605c6980ea00e5
Dimitrii Lopanov <sexykdi@gmail.com>
2025-06-18 17:36:48 +0300

    separate tests for conflicts

--- src/conflicts/mod.rs
+++ src/conflicts/mod.rs
@@ -1,6 +1,7 @@
 //! Export contents of `conflicts` folder
 mod conflicts;
+mod conflicts_test;
 mod conflict_rule;
 mod conflicts_solver;
 
-pub use self::{conflicts::*, conflict_rule::*, conflicts_solver::*};
+pub use self::{conflicts::*, conflicts_test::*, conflict_rule::*, conflicts_solver::*};
